ca1e73cd99105965b8895d58ff37cb5c70a9c9c4
westleyxjames <westleyjames7899@gmail.com>
2026-03-18 15:57:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 <html lang="en">
 <head>
    
-       <!-- Histats.com  START  (aync)-->
+   <!-- Histats.com  START  (aync)-->
 <script type="text/javascript">var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,5009691,4,0,0,0,00010000']);
+_Hasync.push(['Histats.start', '1,5015150,4,0,0,0,00010000']);
 _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
 (function() {
@@ -14,7 +14,7 @@ var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async
 hs.src = ('//s10.histats.com/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
 })();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5009691&101" alt="counter hit xanga" border="0"></a></noscript>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5015150&101" alt="free web site hit counter" border="0"></a></noscript>
 <!-- Histats.com  END  -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
